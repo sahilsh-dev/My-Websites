63ea9ee94e80d2a291a87460eb4866d692b4bb5d
Sam6900 <85671637+Sam6900@users.noreply.github.com>
2022-12-26 00:18:32 +0530

    Added delete and add movie feature

--- Top 10 Movies Website/forms.py
+++ Top 10 Movies Website/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import FloatField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class RateMovieForm(FlaskForm):
@@ -9,3 +9,9 @@ class RateMovieForm(FlaskForm):
     review = StringField("Your Review",
                          validators=[DataRequired()])
     submit = SubmitField("Done")
+
+
+class AddMovieForm(FlaskForm):
+    title = StringField("Movie Title",
+                        validators=[Length(5, 50)])
+    submit = SubmitField("Add Movie")
